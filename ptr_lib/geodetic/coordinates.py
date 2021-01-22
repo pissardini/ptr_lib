2cf72e7b311299c6bcabb95252a2e69f795f2d0c
Rodrigo Pissardini <rodrigo@pissardini.com>
2021-01-22 17:10:20 -0300

    Update coordinates.py
    Inserting llh->utm and utm->llh. Starting the use of type checking.

--- ptr_lib/geodetic/coordinates.py
+++ ptr_lib/geodetic/coordinates.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1252 -*-
 
 #
-# Copyright (c) 2012-2020 R.Pissardini <rodrigo AT pissardini DOT com>
+# Copyright (c) 2012-2021 R.Pissardini <rodrigo AT pissardini DOT com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,23 +21,29 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from math import *
-import datetime
+# 22/01/2021  - insert llh2utm and utm2llh
 
 
-def polar_coordinates (x, y, distance, angle):
+from math import asin, asinh, atanh, atan, atan2, cos, cosh, degrees, fabs, floor, pi, radians, sin, sinh, sqrt, tan,tanh
+from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
+
+def polar_coordinates (coordinate:Tuple[float], 
+                       distance:float, 
+                       angle:float)->Tuple[float]:
     '''
         Calculate polar coordinates from xy, distance and angle (degrees)
     '''
+    x = coordinate[0]
+    y = coordinate[1]
     xf = (distance * cos(radians(angle))) + x
     yf = (distance * sin(radians(angle))) + y
     return (xf,yf)
 
-def lenght_latitude_longitude(value):
+def lenght_latitude_longitude(latitude:float)->Tuple[float]:
     '''
         Lenght of latitude and longitude (degrees)
     '''
-    lat = radians(value)
+    lat = radians(latitude)
     m1 = 111132.92
     m2 = -559.82
     m3 = 1.175
@@ -51,16 +57,16 @@ def lenght_latitude_longitude(value):
     longlen = (p1 * cos(lat)) + (p2 * cos(3 * lat)) +\
             (p3 * cos(5 * lat))
 
-    return (latlen,longlen)
+    return (latlen, longlen)
 
-def quat2euler(qw,qx,qy,qz):
+def quat2euler(qw:float,
+               qx:float,
+               qy:float,
+               qz:float)->Tuple[float]:
     '''
         Convert quaternions to Euler's angles
     '''
-    qw2 = qw * qw
-    qx2 = qx * qx
-    qy2 = qy * qy
-    qz2 = qz * qz
+
     test= qx * qy + qz * qw
 
     y = 0.0
@@ -71,13 +77,13 @@ def quat2euler(qw,qx,qy,qz):
         y = 360/pi * atan2(qx,qw)
         z = 90
         x = 0
-        return [X,Y,Z]
+        return (x,y,z)
 
     if (test < -0.499):
         y = -360/pi*atan2(qx,qw)
         z = -90
         x = 0
-        return (X,Y,Z)
+        return (x,y,z)
 
     h     = atan2(2 * qy * qw - 2 * qx * qz, 1 - 2 * qy * qy - 2 * qz * qz)
     a     = asin (2 * qx * qy + 2 * qz * qw)
@@ -89,7 +95,7 @@ def quat2euler(qw,qx,qy,qz):
     return (x, y, z)
 
 
-def euler2quat(x,y,z):
+def euler2quat(x:float,y:float,z:float)->Tuple[float]:
     '''
         Convert Euler's angles to quaternions
     '''
@@ -108,22 +114,26 @@ def euler2quat(x,y,z):
     qz = ((c1 * s2 * c3 - s1 * c2 * s3)* 100000)/100000
     return (qw, qx, qy, qz)
 
-def rotation_coordinates(x, y, angle):
+def rotation_coordinates(coordinate:Tuple[float], 
+                         angle:float)-> Tuple[float]:
     '''
         Rotation of coordinates
     '''
+    x = coordinate[0]
+    y = coordinate[1]
     xf =  x * cos(radians(angle))+ y * sin(radians(angle))
     yf = -x * sin(radians(angle))+ y * cos(radians(angle))
     return (xf,yf)  
 
-def geodetic2cartesian(lat,
-                       lon,
-                       h,
-                       a = 6378137,
-                       b= 6356752.314140347):
+def geodetic2cartesian(coordinate:Tuple[float],
+                       a:float = 6378137,
+                       b:float = 6356752.314140347)->Tuple[float]:
     '''
         Convert from LLH to ECEF
     '''
+    lat = coordinate[0]
+    lon = coordinate[1]
+    h   = coordinate[2]
     e2 = (pow(a,2) -pow(b,2))/pow(a,2)
     n  = a/(pow(1. -e2 * pow(sin(radians(lat)),2), 0.5))
     x  = (n+h) * cos(radians(lat)) * cos(radians(lon))
@@ -131,14 +141,16 @@ def geodetic2cartesian(lat,
     z  = ((1.-e2) * n + h) * sin(radians(lat))
     return (x,y,z)
  
-def cartesian2geodetic (x,
-                        y,
-                        z,
-                        a = 6378137,
-                        b = 6356752.314140347):
+def cartesian2geodetic (coordinate:Tuple[float],
+                       a:float = 6378137,
+                       b:float = 6356752.314140347)->Tuple[float]:
     '''
         Convert from ECEF to LLH
     '''
+    x = coordinate[0]
+    y = coordinate[1]
+    z   = coordinate[2]
+
     h    = 0.0
     v    = 0.0
     e2   = (pow(a,2) -pow(b,2))/pow(a,2)
@@ -156,14 +168,16 @@ def cartesian2geodetic (x,
     lon = degrees(atan2(radians(y), radians(x)))
     return (lat,lon,h)
 
-def geodetic2enu (lat,
-                  lon,
-                  h,
-                  a = 6378137,
-                  b = 6356752.314140347):
+def geodetic2enu (coordinate:Tuple[float],
+                  a:float = 6378137,
+                  b:float = 6356752.314140347)->Tuple[float]:
     '''
         Convert from LLH to ENU
     '''
+    lat = coordinate[0]
+    lon = coordinate[1]
+    h   = coordinate[2]
+
     e2           = (pow(a,2) - pow(b,2))/pow(a,2)
     lat          = radians(lat)
     v            = a/pow((1- e2* pow(sin(lat),2)),0.5)
@@ -178,18 +192,18 @@ def geodetic2enu (lat,
     return (e,n,u)
     
 
-def helmert_transformation (x,
-                            y,
-                            z,
-                            tx,
-                            ty,
-                            tz,
-                            s,
-                            rx,
-                            ry,
-                            rz,
-                            a = 6378137,
-                            b = 6356752.314140347):
+def helmert_transformation (x:float,
+                            y:float,
+                            z:float,
+                            tx:float,
+                            ty:float,
+                            tz:float,
+                            s:float,
+                            rx:float,
+                            ry:float,
+                            rz:float,
+                            a:float = 6378137,
+                            b:float = 6356752.314140347)->Tuple[float]:
     '''
         Helmert Transformation
     '''
@@ -199,38 +213,324 @@ def helmert_transformation (x,
     
     return (xp,yp,zp)
 
-def sad2sirgas(x,y,z):
+def sad2sirgas(coordinates:Tuple[float])->Tuple[float]:
     '''
         Convert from SAD69 to SIRGAS 2000
     '''
+    x = coordinates[0]
+    y = coordinates[1]
+    z = coordinates[2]
+
     xf = x - 67.35
     yf = y + 3.88
     zf = z - 38.22
     return (xf,yf,zf)
 
-def sirgas2sad(x,y,z):
+def sirgas2sad(coordinates:Tuple[float])->Tuple[float]:
     '''
         Convert from SIRGAS200 to SAD69
     '''
+    x = coordinates[0]
+    y = coordinates[1]
+    z = coordinates[2]
+
     xf = x + 67.35
     yf = y - 3.88
     zf = z + 38.22
     return (xf,yf,zf)
 
-def corregoalegre2sirgas(x,y,z):
+def corregoalegre2sirgas(coordinates:Tuple[float])->Tuple[float]:
     '''
         Convert from Corrego Alegre to SIRGAS
     '''
+    x = coordinates[0]
+    y = coordinates[1]
+    z = coordinates[2]
+
     xf = x - 206.048
     yf = y + 168.279
     zf = z - 3.283
     return (xf,yf,zf)
 
-def sirgas2corregoalegre(x,y,z):
+def sirgas2corregoalegre(coordinates:Tuple[float])->Tuple[float]:
     '''
         Convert from SIRGAS to Corrego Alegre
     '''
+    x = coordinates[0]
+    y = coordinates[1]
+    z = coordinates[2]
+
     xf = x + 206.048
     yf = y - 168.279
     zf = z + 3.283
     return (xf,yf,zf)
+
+def llh2utm(coordinates:Tuple[float],
+            a:float = 6378137,
+            f:float = 1/298.257223563)->Dict:
+    '''
+        Convert from lat, lon, h to utm
+        Based on https://www.movable-type.co.uk/scripts/latlong-utm-mgrs.html
+        
+        Keyword arguments:
+            coordinates  -- (lat, lon, h)
+            a  -- Earth's radius (default:6378137.0)
+            f  -- flattening (default:1/298.257223563)
+        Output:        
+	        dictionary -- {
+                zone: UTM zone,
+                coordinate: tuple with utm coordinate (easting, northing),
+                convergence: meridian convergence (bearing of grid north clockwise from true north), in degrees.
+                scale: grid scale factor
+            }
+
+    '''
+    latitude  = coordinates[0]
+    longitude = coordinates[1]
+
+    assert latitude > -90.0 and latitude < 90.0 and longitude > -180.0 and longitude <= 180.0, 'Insert correct coordinates'
+
+    #calculate longitude zone
+    if longitude < 0.0:
+        zone = floor(((180.0 + longitude) / 6) + 1)
+    else:
+        zone = floor((longitude / 6) + 31)
+    
+    #longitude of central meridian
+    longitude_0           = radians((zone-1)*6 - 180 + 3)
+
+    ## handling Norway/Svalbard exceptions
+
+    mgrsLatBands       = 'CDEFGHJKLMNPQRSTUVWXX'
+    latBand            = mgrsLatBands[floor(latitude/8+10)]
+    rad_six            = radians(6)
+    
+    #adjusting zone and central meridian for Norway
+    if zone==31 and latBand=='V' and longitude>= 3:
+        zone     += 1
+        longitude_0 += rad_six 
+ 
+    #adjust zone & central meridian for Svalbard
+    if zone==32 and latBand=='X' and longitude<9:
+        zone     -= 1
+        longitude_0 -= rad_six 
+
+    if zone==32 and latBand=='X' and longitude>= 9:
+        zone     += 1
+        longitude_0 += rad_six 
+
+    if zone==34 and latBand=='X' and longitude< 21:
+        zone     -= 1
+        longitude_0 -= rad_six 
+    if zone==34 and latBand=='X' and longitude>=21:
+        zone     += 1
+        longitude_0 += rad_six 
+    if zone==36 and latBand=='X' and longitude< 33:
+        zone     -= 1
+        longitude_0 -= rad_six 
+    if zone==36 and latBand=='X' and longitude>=33:
+        zone     += 1
+        longitude_0 += rad_six 
+
+    lat       = latitude
+    latitude  = radians(latitude)
+    longitude = radians(longitude) -longitude_0
+
+    # easting, northing: Karney 2011 Eq 7-14, 29, 35:
+
+    e         = sqrt(f*(2-f)) #eccentricity
+    n         = f / (2 - f)   #3rd flattening 
+    n2        = pow(n,2)
+    n3        = pow(n,3)
+    n4        = pow(n,4)
+    n5        = pow(n,5)
+    n6        = pow(n,6)
+
+    cos_l     = cos(longitude)
+    sin_l     = sin(longitude)
+    tan_l     = tan(longitude)
+
+    tau       = tan(latitude)
+    sigma     = sinh(e* atanh(e* tau/ sqrt(1+ tau*tau)))
+    tau_p     = tau * sqrt(1+sigma * sigma) - sigma * sqrt(1+ tau * tau)
+
+    xi_p      = atan2(tau_p, cos_l)
+    eta_p     = asinh(sin_l / sqrt(tau_p*tau_p + cos_l*cos_l))
+
+    A         = a/(1+n) * (1 + 1/4*n2 + 1/64*n4 + 1/256*n6)
+
+    alpha     = [None, 
+                    1/2*n - 2/3*n2 + 5/16*n3 +   41/180*n4 - 127/288*n5 + 7891/37800*n6,
+                    13/48*n2 -  3/5*n3 + 557/1440*n4 + 281/630*n5 - 1983433/1935360*n6,
+				    61/240*n3 -  103/140*n4 + 15061/26880*n5 + 167603/181440*n6,
+					49561/161280*n4 - 179/168*n5 + 6601661/7257600*n6,
+					34729/80640*n5 - 3418889/1995840*n6,
+                    212378941/319334400*n6 ]
+
+    xi        = xi_p
+    
+    for j in range(1,7,1):
+            xi += alpha[j] * sin(2*j*xi_p) * cosh(2*j*eta_p)
+
+    eta       = eta_p
+
+    for j in range(1,7,1):
+        eta += alpha [j] * cos(2*j*xi_p) * sinh(2*j*eta_p)
+
+    x         = 0.9996 * A * eta #0.996 = UTM scale on the central meridian
+    y         = 0.9996 * A * xi
+
+    # convergence: Karney 2011 Eq 23, 24
+    p_p       = 1
+    
+    for j in range(1,7,1):
+        p_p += 2*j*alpha[j] * cos(2*j*xi_p) * cosh(2*j*eta_p)
+
+    q_p       = 0
+    
+    for j in range(1,7,1):
+    	q_p += 2*j*alpha[j] * sin(2*j*xi_p) * sinh(2*j*eta_p)
+
+    gamma_p  = atan(tau_p / sqrt(1+tau_p*tau_p)*tan_l)
+    gamma_pp = atan2(q_p, p_p)
+    gamma    = gamma_p + gamma_pp
+
+    # scale: Karney 2011 Eq 25
+
+    sin_lat = sin(latitude)
+    k_p = sqrt(1 - e*e*sin_lat*sin_lat) * sqrt(1 + tau * tau) / sqrt(tau_p*tau_p + cos_l*cos_l)
+    k_pp = A / a * sqrt(p_p*p_p + q_p*q_p)
+
+    k = 0.9996 * k_p * k_pp
+
+    x = x + 500e3
+    if (y < 0):
+        y = y + 10000e3
+
+    return {'zone':zone,
+            'coordinate':(round(x,9),round(y,9),"N" if lat>0 else "S"),
+            'convergence': round(degrees(gamma),9),
+            'scale': round(k,12)
+            }
+
+def utm2llh(
+            zone:float,
+            coordinate:Tuple[float],
+            a:float = 6378137, 
+            f:float = 1/298.257223563)->Dict:
+    
+    '''
+        Convert from utm to latlon
+        Based on https://www.movable-type.co.uk/scripts/latlong-utm-mgrs.html
+        
+        Keyword arguments:
+            zone         -- UTM zone
+            coordinate  -- (easting,northing,hemisphere)
+            a  -- Earth's radius (default:6378137.0)
+            f  -- flattening (default:1/298.257223563)        
+        Output:        
+	        dictionary -- {
+                coordinate: tuple with latlon coordinates
+                convergence: meridian convergence (bearing of grid north clockwise from true north), in degrees.
+                scale: grid scale factor
+            }
+
+    '''
+    
+    easting    = coordinate[0]
+    northing   = coordinate[1]
+    h          = coordinate[2]
+
+    x          = easting - 500e3
+    if h=='S':
+        y = northing - 10000e3
+    else:
+        y = northing
+    #
+    
+    # from Karney 2011 Eq 15-22, 36
+
+    e         = sqrt(f*(2-f)) #eccentricity
+    n         = f / (2 - f)   #3rd flattening 
+    n2        = pow(n,2)
+    n3        = pow(n,3)
+    n4        = pow(n,4)
+    n5        = pow(n,5)
+    n6        = pow(n,6)
+    
+    A         = a/(1 + n) * (1 + 1/4 * n2 + 1/64 * n4 + 1/256 * n6)
+    eta       = x /(0.9996 * A)
+    xi        = y /(0.9996 * A)
+
+    beta = [None,
+                1/2 * n - 2/3 * n2 + 37/96 * n3 - 1/360 * n4 - 81/512 * n5 + 96199/604800 * n6,
+                1/48*n2 + 1/15*n3 - 437/1440 * n4 + 46/105*n5 - 1118711/3870720*n6,
+                17/480*n3 - 37/840 * n4 - 209/4480 * n5 + 5569/90720*n6,
+                4397/161280 * n4 - 11/504 * n5 - 830251/7257600 * n6,
+                4583/161280 * n5 - 108847/3991680 * n6,
+                20648693/638668800 * n6]
+
+    xi_p = xi
+    
+    for j in range(1,6,1):
+        xi_p -= beta[j] * sin(2*j*xi) * cosh(2*j*eta)
+
+    eta_p = eta
+    
+    for j in range(1,6,1):
+        eta_p -= beta[j] * cos(2*j*xi) * sinh(2*j*eta)
+
+    sinh_eta_p  = sinh(eta_p)
+    sin_xi_p    = sin(xi_p)
+    cos_xi_p    = cos(xi_p)
+
+    tau_p       = sin_xi_p / sqrt(sinh_eta_p * sinh_eta_p + cos_xi_p * cos_xi_p)
+
+    delta_tau_i = 1e12
+    tau_i       = tau_p
+    
+    while (abs(delta_tau_i) > 1e-12):
+        sigma_i  = sinh(e * atanh(e * tau_i/ sqrt(1+tau_i*tau_i)))
+        tau_i_p  = tau_i * sqrt(1 + sigma_i*sigma_i) - sigma_i * sqrt(1 + tau_i * tau_i)
+        delta_tau_i = (tau_p - tau_i_p)/sqrt(1+tau_i_p * tau_i_p) *\
+                      (1 + (1-e * e)* tau_i * tau_i) / ((1-e*e)*sqrt(1+tau_i*tau_i))
+        tau_i += delta_tau_i
+    
+    tau     = tau_i
+    phi     = atan(tau)
+    lambda_ = atan2(sinh_eta_p, cos_xi_p)
+
+    #convergence: Karney 2011 Eq 26, 27
+    p = 1
+    
+    for j in range(1,7,1):
+        p -= 2 * j * beta[j] * cos(2*j*xi) * cosh(2 * j * eta)
+        
+    q = 0
+    for j in range(1,7,1):
+        q += 2 * j * beta[j] * sin(2 * j * xi) * sinh(2 * j * eta)
+
+    gamma_p  = atan(tan(xi_p) * tanh(eta_p))
+    gamma_pp = atan2(q, p)
+    gamma    = gamma_p + gamma_pp
+
+    #Karney 2011 Eq 28
+
+    sin_phi = sin(phi)
+    k_p = sqrt(1 - e * e * sin_phi * sin_phi) * sqrt(1 + tau * tau) *\
+          sqrt(sinh_eta_p * sinh_eta_p + cos_xi_p * cos_xi_p)
+    
+    k_pp = A / a / sqrt(p*p + q*q)
+
+    k = 0.9996 * k_p * k_pp
+
+
+    lambda_0 = radians((zone-1)*6 - 180 + 3)
+    lambda_ += lambda_0
+    
+    return {
+            'coordinate': (round(degrees(phi),14),
+                           round(degrees(lambda_),14)),
+            'convergence': round(degrees(gamma),9),
+            'scale': round(k,12),               
+            }
